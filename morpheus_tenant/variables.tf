7dc7a5dd3522f7ec57f3b296e593db9ada91e3c5
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-03-10 19:04:24 +0000

    Update variables.tf

--- morpheus_tenant/variables.tf
+++ morpheus_tenant/variables.tf
@@ -12,3 +12,18 @@ variable "morpheus_password" {
   description = "The password of the user account used to access the Morpheus platform"
   type        = string
 }
+
+variable "tenant_name" {
+  description = "The name of the tenant being created on Morpheus platform"
+  type        = string
+}
+
+variable "tenant_description" {
+  description = "The description of the tenant being created on Morpheus platform"
+  type        = string
+}
+
+variable "tenant_currency" {
+  description = "The currency of the tenant being created on Morpheus platform"
+  type        = string
+}
